df3687eb2193f478f3f2b760f44c911242c0fb3e
nitram509 <maki@bitkings.de>
2013-08-02 16:34:53 +0200

    added single thread model sample

---
+++ chapter_2/compute.js
@@ -0,0 +1,11 @@
+function schwierigeBerechnung() {
+
+  var now = new Date().getTime();
+
+  console.log("start");
+  while (new Date().getTime() < (now + 1000 * 10)) {
+    // idle
+  }
+  console.log("fertig");
+
+}
