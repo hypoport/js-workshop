c10e95fdf58bed5916ef5b8de55a994cecac0dcb
Martin Kirst <martin.kirst@hypoport.de>
2013-08-15 15:58:13 +0200

    improved 02_vanilla_dom.html with more functions

--- chapter_3/vanilla_dom_functions.js
+++ chapter_3/vanilla_dom_functions.js
@@ -1,7 +1,22 @@
-
-function ermittle_Element_per_ID(name) {
-  var element = document.getElementById(name);
+function ermittle_Element_per_ID(id) {
+  var element = document.getElementById(id);
   console.info("Name des Elements: " + element.nodeName);
   console.info("Typ des Elements: " + element.nodeType);
   console.info("Wert des Elements: " + element.nodeValue);
+  return element;
+}
+
+function erstelle_neues_Element(text) {
+  var element_li = document.createElement("li")
+  var element_text = document.createTextNode(text);
+  element_li.appendChild(element_text);
+  return element_li;
+}
+
+function erzeuge_neues_KindElement(parentId, text) {
+  var parent = ermittle_Element_per_ID(parentId);
+  if (parent) {
+    var child = erstelle_neues_Element(text);
+    parent.appendChild(child);
+  }
 }
