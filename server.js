128ed1ef9ea28cc8fe416b662c54ded6c1c72ca8
Andreas Marek <andimarek@fastmail.fm>
2013-08-30 08:50:34 +0200

    chapeter 5 weiter gemacht

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.put('/jquery-put-url', function (req, res) {
 });
 
 app.put('/jquery-json-url', function (req, res) {
-  res.send({hall:"welt"});
+  res.send({hallo: "welt"});
+});
+
+app.put('/jquery-json-url', function (req, res) {
+  res.send({hallo: "welt"});
 });
 console.log('Started server at port 8080');
